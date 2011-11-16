e80f027332a50efc0379aa1a54cce3a1226f4d10
Don Park <don.park@gmail.com>
2011-11-16 14:00:28 -0800

    more config ports

--- bouncy.js
+++ bouncy.js
@@ -5,9 +5,9 @@ console.log("bouncy listening on "+settings.bouncy.listen_port)
 bouncy(function (req, bounce) {
 console.log(req.url)
     if (req.url.match(/^\/socket.io\//)) {
-        bounce(8080);
+        bounce(settings.socket_io.listen_port);
     }
     else {
-        bounce(3000)
+        bounce(settings.web.listen_port)
     }
 }).listen(settings.bouncy.listen_port);
