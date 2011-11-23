8955e24ec78304c8747d0639b93c61dc30815dd7
Don Park <don@donpark.org>
2011-11-23 12:47:37 -0800

    iss watcher

---
+++ iss-now.js
@@ -0,0 +1,32 @@
+"use strict"
+var timers = require('timers')
+var net = require('net')
+var rest = require('restler')
+var settings = require('./settings')
+var client
+
+client = net.connect(settings.api.listen_port, "donpark.org", start_timer)
+
+function start_timer() {
+  console.log('api connected. start timer')
+  timers.setInterval(function() {
+      iss_request(iss_position);
+    }, 5000)
+}
+
+function iss_request(callback) {
+  var url='http://api.open-notify.org/iss-now/'
+  console.log(url)
+  rest.get(url).on('complete', callback)
+}
+
+function iss_position(request) {
+  var msg = { type:"location",
+              username:"iss",
+              position: { latitude:request.iss_position.latitude,
+                          longitude:request.iss_position.longitude}
+            }
+  var msg_s = JSON.stringify(msg)+"\n"
+  console.log(msg_s)
+  client.write(msg_s)
+}
