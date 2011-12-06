c516c8537edacb58e6f32a63b169b6b5bd325403
Don Park <don@donpark.org>
2011-12-06 09:50:34 -0800

    stay running when the destination is closed

--- bouncy.js
+++ bouncy.js
@@ -3,11 +3,15 @@ var bouncy = require('bouncy')
 
 console.log("bouncy listening on "+settings.bouncy.listen_port)
 bouncy(function (req, bounce) {
-console.log(req.url)
-    if (req.url.match(/^\/socket.io\//)) {
-        bounce(settings.socket_io.listen_port);
-    }
-    else {
-        bounce(settings.web.listen_port)
-    }
+  console.log(req.url)
+  if (req.url.match(/^\/socket.io\//)) {
+      bounce(settings.socket_io.listen_port).on('error', errlog);
+  }
+  else {
+      bounce(settings.web.listen_port).on('error', errlog);
+  }
 }).listen(settings.bouncy.listen_port);
+
+function errlog(e) {
+  console.log(''+e)
+}
