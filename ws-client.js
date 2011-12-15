708872d905e01890a8c4ae23fb02b474804ff7ba
Don Park <don@donpark.org>
2011-12-15 12:42:14 -0800

    cli and browser websockets test clients

---
+++ ws-client.js
@@ -0,0 +1,18 @@
+var websockets = require('websockets');
+var settings = require('./settings');
+
+var url = 'ws://127.0.0.1:2040'
+var ws = new websockets.WebSocket(url);
+
+ws.on('open', function() {
+  console.log('ws open.'+url);
+});
+
+ws.on('message', function(data) {
+  console.log('-> '+data)
+});
+
+ws.on('close', function() {
+  console.log('ws close');
+});
+
