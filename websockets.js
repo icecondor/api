c60956fe0d263341e4fd103a9128a1d5adad2b30
Don Park <don@donpark.org>
2011-12-15 13:29:58 -0800

    websockets is out, websock is in

--- websockets.js
+++ websockets.js
@@ -1,27 +1,22 @@
 var net = require('net');
-var ws_svr = require('websockets');
+var wsock = require('websock');
 var settings = require('./settings');
 
-var ws = ws_svr.createServer(ws_http);
-ws.on('connect', ws_connect).listen(settings.websockets.listen_port);
+wsock.listen(settings.websockets.listen_port, ws_connect);
 console.log("websockets listening on "+settings.websockets.listen_port)
 
-function ws_http(req, res) {
-  console.log('http connection')
-}
-
 function ws_connect(socket) {
+  console.log('ws_connect')
   var apiSocket;
 
-  socket.on('open', function() {
-    console.log('ws open. connecting to '+settings.api.listen_port);
-    apiSocket = net.connect(settings.api.listen_port, "localhost")
-    apiSocket.on('data', function(data) {
-      console.log('-> '+data)
-      socket.send(data)
-    })
+  console.log('ws open. connecting to '+settings.api.listen_port);
+  apiSocket = net.connect(settings.api.listen_port, "localhost")
 
-  });
+  apiSocket.on('data', function(data) {
+    console.log('-> '+data)
+    socket.send(data)
+  })
+
   socket.on('message', function(data) {
     console.log('<- '+data)
     apiSocket.write(data+"\n")
@@ -31,27 +26,5 @@ function ws_connect(socket) {
     apiSocket.end();
     console.log('ws close');
   });
+
 }
-
-/*// socket.io
-io.sockets.on('connection', function (client) {
-  console.log(client.id+" connecting to API")
-  var apiSocket = net.connect(settings.api.listen_port, "localhost")
-
-  apiSocket.on('data', function(data) {
-    var msg = JSON.parse(data.toString('utf8'))
-    client.emit('update',msg)
-  })
-
-  client.on('following', function (msg) {
-    var data = JSON.stringify(msg)
-    apiSocket.write(data+"\n")
-  });
-
-  client.on('disconnect', function(client) {
-    apiSocket.end()
-    console.log('disconnnect!')
-  })
-});
-
-*/
--- ws-client.js
+++ ws-client.js
@@ -1,15 +1,27 @@
-var websockets = require('websockets');
+var wsock = require('websock');
+var readline = require('readline');
 var settings = require('./settings');
 
 var url = 'ws://127.0.0.1:2040'
-var ws = new websockets.WebSocket(url);
+var ws = new wsock.connect(url);
+
+var rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('=> ')
 
 ws.on('open', function() {
-  console.log('ws open.'+url);
+  console.log('ws open '+url);
+  rl.prompt()
+  rl.on('line', function(answer){
+    ws.send(answer); 
+    rl.prompt()}).on('close', function() {
+                       console.log('exiting');
+                       process.exit(0);
+                     });
 });
 
 ws.on('message', function(data) {
   console.log('-> '+data)
+  rl.prompt()
 });
 
 ws.on('error', function(data) {
@@ -19,4 +31,3 @@ ws.on('error', function(data) {
 ws.on('close', function() {
   console.log('ws close');
 });
-
