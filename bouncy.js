69e8adf6c4993331622973d977303345435b41ec
Don Park <don.park@gmail.com>
2011-12-30 12:08:34 -0800

    forward api requests

--- bouncy.js
+++ bouncy.js
@@ -2,13 +2,21 @@ var bouncy = require('bouncy')
   , settings = require('./settings');
 
 console.log("bouncy listening on "+settings.bouncy.listen_port)
+
 bouncy(function (req, bounce) {
   console.log(req.url)
-  if (req.url.match(/^\/socket.io\//)) {
+
+  if (host.match(/^(www\.)?icecondor\.com/)) {
+    if (req.url.match(/^\/socket.io\//)) {
       bounce(settings.socket_io.listen_port).on('error', errlog);
-  }
-  else {
+    }
+    else {
       bounce(settings.web.listen_port).on('error', errlog);
+    }
+  }
+
+  if (host.match(/^api\.icecondor\.com/)) {
+    bounce(settings.websockets.listen_port);
   }
 }).listen(settings.bouncy.listen_port);
 
