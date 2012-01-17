7b7fea8c99c8427dd47df39b07e0a050d56a40aa
Don Park <don.park@gmail.com>
2012-01-17 02:42:09 -0800

    trimet tweaks

--- trimet-now.js
+++ trimet-now.js
@@ -6,11 +6,12 @@ var request = require('request'),
     
 var trimet = JSON.parse(fs.readFileSync('trimet.json'));
 
+console.log('connecting to icecondor api on '+settings.api.listen_port)
+client = net.connect(settings.api.listen_port, "localhost", start_timer)
+
 var url = "http://developer.trimet.org/ws/V1/arrivals?locIDs="+trimet.stopID
            +"&json=yes&appID="+trimet.appID
 console.log(url)
-
-client = net.connect(settings.api.listen_port, "localhost", start_timer)
 
 function start_timer() {
   console.log('api connected. start timer')
