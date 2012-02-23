3aba408305503bfe374c7b0e6989702e1ca2514b
Don Park <don@donpark.org>
2012-02-23 21:34:55 +0700

    play a fixed gpx track into icecondor

---
+++ gpxplay.js
@@ -0,0 +1,52 @@
+"use strict"
+var timers = require('timers')
+var fs = require('fs')
+var net = require('net')
+var xml = require('libxmljs')
+var path = require('path')
+var settings = require('./settings')
+var util = require('util');
+var username;
+var client = net.connect(settings.api.listen_port, "localhost", play)
+
+function play() {
+  var filename = process.argv[2]
+  username = path.basename(filename, '.gpx')
+  var xmlDoc = xml.parseXmlString(fs.readFileSync(filename,'utf8'));
+  var points = xmlDoc.find("//trkseg/trkpt");
+  console.log("** loaded "+points.length+" points from "+filename)
+  console.log("")
+
+  while(true) {
+    for(var f in points) {
+      position(points[f].attr('lat').value(),
+               points[f].attr('lon').value())
+      sleep(5000)
+    }
+  }
+}
+
+// its ok we own the whole node process :)
+function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+}
+
+function position(lat, lng) {
+  var msg = { type:"location",
+              username: username,
+              date: new Date(),
+              provider: "api",
+              position: { latitude:lat,
+                          longitude:lng}
+            }
+  cwrite(client, msg)            
+}
+
+function cwrite(client, msg) {
+  var msg_s = JSON.stringify(msg)+"\n"
+  console.log(msg_s)
+  client.write(msg_s)
+}
+
+play('path.gpx')
