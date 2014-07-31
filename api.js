aad87389d5c138f10ceb21bfe00587e218e122c3
Don Park <donp@donp.org>
2014-07-31 11:20:44 -0700

    more fully formed response

--- api.js
+++ api.js
@@ -337,7 +337,9 @@ function finish_auth(_,result, cred, client) {
 }
 
 function user_detail(client, params) {
-  client_write(client, {id: params.id, result: {id:"ab14", user:"bob"}})
+  var msg = {id: params.id, result: {id:"ab14", user:"bob"}}
+  client_write(client, msg)
+  clog(me,"user_detail-> "+JSON.stringify(msg))
 }
 
 
