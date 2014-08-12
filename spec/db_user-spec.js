e4d3d8fc2cb401ad2800bdf4b69d833b351a699e
Don Park <donp@donp.org>
2014-08-12 11:13:23 -0700

    not sure how far this mock rabbit hole goes

--- spec/db_user-spec.js
+++ spec/db_user-spec.js
@@ -1,13 +1,14 @@
-var rethink_mock = require('../spec/rethink_mock')
+var rethink_mock = require('../spec/rethink_mock').seed({
+  users: ["bob@server"]
+})
 var db = require('../lib/dblib').factory(rethink_mock)
 
 describe("data users", function(){
-  it("should search for user by email", function() {
+  it("should search for user by email", function(done) {
     db.setup(function(){
-      expect(1).toEqual(2)
       db.find_user_by_email('bob@server', function(user){
         console.log("got "+user)
-        expect(user).toEqual("a")
+        expect(user).toEqual("bob@server")
         done()
       })
     })
--- spec/rethink_mock.js
+++ spec/rethink_mock.js
@@ -2,9 +2,64 @@ var Promise = require('bluebird');
 
 module.exports = (function() {
   var mock = {}
+  var existingDBs = []
+  var seed
+
+  mock.seed = function(_seed) {
+    seed = _seed
+    return mock
+  }
+
+  function factoryRunAnswer(value){
+    return { run: function(a,b){
+        return new Promise(function(resolve, reject){
+          if(b) {b(null, value)}
+          resolve(value)
+        })
+      }
+    }
+  }
+
   mock.connect = function() {
     console.log('mock db connected')
-    return new Promise(function(){})
+    return factoryRunAnswer(conn).run()
+  }
+
+  mock.dbCreate = function(db_name) {
+    existingDBs.push(db_name)
+    return factoryRunAnswer()
+  }
+
+  mock.dbList = function() {
+    return factoryRunAnswer(existingDBs)
+  }
+
+  mock.tableList = function() {
+    return factoryRunAnswer([])
+  }
+
+  mock.tableCreate = function(name) {
+    return factoryRunAnswer()
+  }
+
+  mock.table = function(name){
+    console.log('mock using table '+name)
+    return tableFactory(seed[name])
+  }
+
+  var conn = {}
+  conn.use = function(name) {
+    console.log('mock using db '+name)
+  }
+
+  function tableFactory(data){
+    var table = {}
+    table.data = data
+
+    table.filter = function(spec) {
+      return factoryRunAnswer({toArray:function(){return table.data}})
+    }
+    return table
   }
 
   return mock
