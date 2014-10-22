57f7301e29ced57773958fdd72049b180fd40517
Don Park <donp@donp.org>
2014-10-21 17:48:30 -0700

    the hard way

--- couch2rethink.js
+++ couch2rethink.js
@@ -6,17 +6,65 @@ var JSONStream = require('JSONStream')
 
 var echoStream = new stream.Writable({objectMode: true});
 
+var user_docs = require('./users.json')
+console.log('users', user_docs.length)
+var users = {}
+
+
+function transform_user(user) {
+  var new_user = {
+          id: user._id,
+          email: user.email,
+          username: user.username,
+          created_at: user.created_at,
+          devices: [],
+          friends: [],
+          access: {}
+        }
+  return new_user
+}
+
 function transform(loc){
+  var user = users[loc.username]
+  if (user) {
     loc.id = loc._id
     delete loc._id
     delete loc._rev
+  } else {
+    console.log('location user unknown!', loc.username)
+  }
 }
 
 r.connect({db:'icecondor'}).then(function(conn){
+  for(var idx in user_docs) {
+    var user = transform_user(user_docs[idx].doc)
+    console.log('transform_user wtf1', idx, user.username)
+    r.table('users').filter({username: user.username}).run(conn,
+      (function(user){
+        return function(err, cursor){
+          console.log('transform_user wtf2', idx, user)
+          cursor.toArray(function(err, ary){
+            console.log('users filter cursor', ary)
+            if(ary.length > 0) {
+              console.log("user exists", ary[0].username)
+              users[ary[0].username] = ary[0]
+            } else {
+              console.log('transform_user insert', user)
+              r.table('users').insert(user).run(conn, function(err, result){
+                console.log("user saved!", result)
+                users[user.username] = user
+              })
+            }
+          })
+        }
+      })(user)
+    )
+  }
+
   echoStream._write = function (chunk, encoding, done) {
     transform(chunk)
 
-    r.table('load').insert(chunk).run(conn, function(doc, result){
+    r.table('activities').insert(chunk).run(conn, function(doc, result){
       if(result.errors > 0) {
         console.log(chunk.type, result.errors)
       }
