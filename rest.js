8df22631038f8db80b28a8afefa670b05e8542d9
Don Park <donp@donp.org>
2017-07-13 14:14:50 -0700

    use http response object like a promise

--- rest.js
+++ rest.js
@@ -1,19 +1,18 @@
 var net = require('net')
 var http = require('http')
 var url = require('url')
-var wsock = require('websock')
 var settings = require('./settings')
 
 console.log("rest listening on", settings.rest.listen_port)
 var server = http.createServer( function(request, response) {
     var pathname = url.parse(request.url).pathname
     console.log('rest connected. ', pathname)
-    push_point("key", {})
+    push_point(response, "key", {})
 })
 
 server.listen(settings.rest.listen_port)
 
-function push_point(auth_token, geopoint) {
+function push_point(response, auth_token, geopoint) {
   console.log('ws_connect')
   var apiSocket = new net.Socket()
 
@@ -21,6 +20,29 @@ function push_point(auth_token, geopoint) {
 
   apiSocket.on('data', function(data) {
     console.log('-> '+data)
+    var msg = JSON.parse(data)
+    if(msg.method) {
+      console.log('method', msg.method)
+      if(msg.method == "hello") {
+        apiSocket.write(JSON.stringify({id:"r1",
+          method:"auth.session", params:{device_key: auth_token}})+"\n")
+      }
+    }
+
+    if(msg.error) {
+      apiSocket.end()
+      response.writeHead(500)
+      response.end()
+    }
+
+    if(msg.result) {
+      if(msg.user) {
+        apiSocket.write(JSON.stringify({id:"l1",
+          method:"location.add", params:{}})+"\n")
+        response.writeHead(200)
+        response.end()
+      }
+    }
   })
 
   apiSocket.on('error', function(exception) {
