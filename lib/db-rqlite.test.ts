717edfa3ea3ad2cea2c0472b7afce2fb4f23583f
Don Park <donp@donp.org>
2018-07-03 09:38:32 -0700

    utf8

--- lib/db-rqlite.test.ts
+++ lib/db-rqlite.test.ts
@@ -1,7 +1,7 @@
 import * as db from '../lib/db-rqlite'
 import * as fs from 'fs'
 
-let settings = JSON.parse(fs.readFileSync("settings.test.json"))
+let settings = JSON.parse(fs.readFileSync("settings.test.json", 'utf8'))
 
 test('connect', () => {
   let rdb = new db.Db(settings.rqlite)
