b5f464ef9c1edd14cf2d33722828272c517ea4fd
Don Park <donp@donp.org>
2018-07-16 10:21:00 -0700

    sql string result

--- lib/db-rqlite.ts
+++ lib/db-rqlite.ts
@@ -107,7 +107,7 @@ export class Db implements DbBase {
     if(!result.values) {
       result.values = []
     }
-    console.log(sql.toString(), result)
+    console.log(sql.toString(), "("+result.values.length+")", result.values[0])
     return result
   }
 
