76214a1485fcc88c769537f76c9b71a0cf290a8e
Don Park <donp@donp.org>
2018-08-21 11:33:46 -0700

    left one out

--- lib/nouns.ts
+++ lib/nouns.ts
@@ -1,8 +1,9 @@
 import { Access } from '../../protobuf/ts/access'
 import { Config } from '../../protobuf/ts/config'
+import { Device } from '../../protobuf/ts/device'
 import { Friendship } from '../../protobuf/ts/friendship'
 import { Heartbeat } from '../../protobuf/ts/heartbeat'
 import { Location } from '../../protobuf/ts/location'
 import { User } from '../../protobuf/ts/user'
 
-export { Access, Config, Friendship, Heartbeat, Location, User }
+export { Access, Config, Device, Friendship, Heartbeat, Location, User }
