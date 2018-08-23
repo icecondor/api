f5b278c75e5968f24ab33ef6f280ad30d7eaf066
Don Park <donp@donp.org>
2018-08-23 13:34:02 -0700

    line at a time

--- migrate/activity2rql.ts
+++ migrate/activity2rql.ts
@@ -8,16 +8,20 @@ rdb.connect(async () => {
   try {
     const filename = process.argv[2]
 
-    // at once
-    const data = fs.readFileSync(filename, 'utf8')
-    const users = JSON.parse(data)
-    console.log(filename, users.length, 'activities')
-    for (const line of users) await dbsave(line)
-
-    // var lineReader = require('readline').createInterface({input: fs.createReadStream(filename)})
-    // lineReader.on('line', function (line) {
-    //   //await dbsave(user, line)
-    // })
+    var lineReader = require('readline').createInterface({input: fs.createReadStream(filename)})
+    lineReader.on('line', async function (line) {
+      try {
+        line = line.replace(/^\[/, '')
+        line = line.replace(/^\]/, '')
+        line = line.replace(/,$/, '')
+        if(line.length > 0) {
+          var act = JSON.parse(line)
+          await dbsave(act)
+        }
+      } catch(e) {
+        console.log('bogus:', line, e)
+      }
+    })
     console.log('done')
   } catch (e) {
     console.log(e)
