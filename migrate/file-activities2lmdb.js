d35a62629825a4ec72d8fe744f4e8fc4360aa993
Don Park <donp@donp.org>
2018-09-07 12:09:42 -0700

    lmdb 3

---
+++ migrate/file-activities2lmdb.js
@@ -0,0 +1,17 @@
+require('source-map-support').install()
+import * as Db from '../lib/db-lmdb'
+import * as fs from 'fs'
+
+let settings = JSON.parse(fs.readFileSync("settings.json", 'utf8'))
+
+console.log('lmdb', settings.lmdb)
+let db = new Db.Db(settings.lmdb)
+
+let filename = process.argv[2]
+console.log('loading', filename)
+let activities = JSON.parse(fs.readFileSync(filename))
+
+for (const activity of activities) {
+  console.log('activity', activity.type)
+  db.activity_add(activity)
+}
