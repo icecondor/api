9c287a0feffbfb3757c06d258e8b2c6b76827e70
Don Park <donp@donp.org>
2018-09-19 10:33:57 -0700

    setup for multi backend

--- websockets.js
+++ websockets.js
@@ -1,37 +1,50 @@
-var net = require('net');
-var wsock = require('websock');
-var settings = require('./settings');
+var net = require('net')
+var wsock = require('websock')
+var settings = require('./settings')
+var apis = []
 
 wsock.listen(settings.websockets.listen_port, ws_connect);
-console.log("websockets listening on "+settings.websockets.listen_port)
+console.log("websockets listening on " + settings.websockets.listen_port)
 
 function ws_connect(socket) {
-  console.log('ws_connect')
+  console.log('websockets open. connecting to api on ' + settings.api.listen_port);
+  var sockApi = relayTo(socket, 'localhost', settings.api.listen_port)
+  if (sockApi) {
+    apis.push(sockApi)
+
+    socket.on('message', function(data) {
+      console.log('['+apis.length+']<-ws ' + data)
+      for(const api in aps) api.write(data + "\n")
+    })
+
+    socket.on('close', function() {
+      console.log('websocket closed. closing api');
+      for(const api in aps) api.end()
+    })
+  } else {
+    console.log('api socket open fail! closing client.')
+    socket.end()
+  }
+}
+
+function relayto(socket, host, port) {
   var apiSocket = new net.Socket();
 
-  console.log('websockets open. connecting to api on '+settings.api.listen_port);
-
   apiSocket.on('data', function(data) {
-    console.log(socket.address+'-> '+data)
+    console.log(':'+socket.connection._peername.port+'-> '+data)
     socket.send(data)
   })
 
   apiSocket.on('error', function(exception) {
-    console.log("apiSocket error: "+exception);
+    console.log("apiSocket error: " + exception);
     apiSocket.end()
     socket.end()
   })
 
-  apiSocket.connect(settings.api.listen_port, "localhost")
+  apiSocket.on('close', function() {
+    socket.end()
+  })
+  apiSocket.connect(port, host)
 
-  socket.on('message', function(data) {
-    console.log('<-ws '+data)
-    apiSocket.write(data+"\n")
-  });
-
-  socket.on('close', function() {
-    apiSocket.end();
-    console.log('websockets close');
-  });
-
+  return apiSocket
 }
