c93fbf0ae68057dbe772715f039f85adb604e776
Don Park <donp@donp.org>
2018-09-20 12:03:19 -0700

    fence records

--- lib/db-lmdb.ts
+++ lib/db-lmdb.ts
@@ -526,7 +526,7 @@ export class Db extends DbBase {
 
   async fence_list(user_id) {
     let kvs = this.getIdxBetween('fence', 'user_id_id', [user_id], [user_id])
-    let values = kvs.map(k => kvs[k])
+    let values = Object.keys(kvs).map(k => kvs[k])
     console.log('fence_list says', user_id, values)
     return { toArray: () => Promise.resolve(values) } // quack like rethinkdb
   }
