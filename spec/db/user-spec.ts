ab0372e92e51f40c010ad6ca69a47df2c88087cf
dpdp <donp@donp.org>
2021-07-26 10:08:57 -0700

    less comments

--- spec/db/user-spec.ts
+++ spec/db/user-spec.ts
@@ -12,16 +12,12 @@ beforeAll(() => {
 describe("existing users", function() {
   it("should search for user by email", async function() {
     return db.connect(async function() {
-      //await db.create_user({ email: "a@b", username: "ab" })
-      //let user = await db.find_user_by({ email: "a@b" })
       await expect(db.find_user_id_by({ email: "a@b" })).resolves.toHaveLength(36)
     })
   })
 
   it("should search for user by email and not find", async function() {
     return db.connect(async function() {
-      //await db.create_user({ email: "a@b", username: "ab" })
-      //let user = await db.find_user_by({ email: "a@b" })
       await expect(db.find_user_id_by({ email: "not@here" })).rejects.toMatch('key not found')
     })
   })
